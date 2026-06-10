de3ae7a4fcbeb4da550954f532bd9ad76cb79891
cristian cardenas <cfcardenas@uniboyaca.co>
2026-06-09 19:29:26 -0500

    Cristian Felipe Cardenas Zorro, estructura replanteada

--- ms-reservas/app/Config/Database.php
+++ ms-reservas/app/Config/Database.php
@@ -1,32 +1,22 @@
 <?php
 
-namespace App\Config;
-
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Dotenv\Dotenv;
 
-class Database
-{
-    public static function initialize(): void
-    {
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-        $dotenv->load();
+$capsule = new Capsule;
 
-        $capsule = new Capsule();
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => $_ENV['DB_HOST'],
+    'port'      => $_ENV['DB_PORT'],
+    'database'  => $_ENV['DB_DATABASE'],
+    'username'  => $_ENV['DB_USERNAME'],
+    'password'  => $_ENV['DB_PASSWORD'],
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+]);
 
-        $capsule->addConnection([
-            'driver'    => 'mysql',
-            'host'      => $_ENV['DB_HOST'],
-            'port'      => $_ENV['DB_PORT'],
-            'database'  => $_ENV['DB_NAME'],
-            'username'  => $_ENV['DB_USER'],
-            'password'  => $_ENV['DB_PASS'],
-            'charset'   => $_ENV['DB_CHARSET'],
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix'    => '',
-        ]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-    }
-}
+return $capsule;
